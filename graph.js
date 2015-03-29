f032675f172f560157b93e460d7fc11f24e26a43
Dominik Ferber <dominik.ferber@gmail.com>
2015-03-29 18:43:25 +0200

    remove non-core functions.

--- graph.js
+++ graph.js
@@ -63,48 +63,5 @@ _.extend(GraphAPI.prototype, {
 
 	post: function (edge, params) {
 		return this._call('POST', edge, params);
-	},
-
-
-	getPage: function (pageId) {
-		var response = this.get([pageId], {
-			access_token: this._getAccessToken()
-		});
-
-		// if response.ok()
-		if (response && response.statusCode === 200) {
-			return response.data;
-		} else {
-			return false;
-		}
-	},
-
-	getEvent: function (eventId, fields) {
-		var response = this.get([eventId], {
-			access_token: this._getAccessToken(),
-			fields: fields || 'id,name,description'
-		});
-
-		return response.ok() ? response.data : false;
-	},
-
-	getEvents: function (pageId, options) {
-		options = options || {};
-		var query = {};
-		query.fields = options.fields || 'id,name,start_time,end_time,cover';
-		query.access_token = this._getAccessToken();
-
-		var response = this.get([pageId, 'events'], query);
-
-		// TODO use query that gets ALL upcoming events
-		return response.ok() ? response.data.data : false;
-	},
-
-	getProfilePictureUrl: function (userId) {
-		var response = this.get([userId, 'picture'], {
-			redirect: false
-		});
-
-		return response.ok() ? response.data.data.url : false;
 	}
 });
