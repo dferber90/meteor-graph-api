7ba0bf70194185c0ae889c15b69f12a7d9b19dfa
Dominik Ferber <dominik.ferber@gmail.com>
2015-04-01 15:23:04 +0200

    add test for to verify edgeArray is not modified.

--- tests/GraphAPI.js
+++ tests/GraphAPI.js
@@ -15,6 +15,11 @@ describe('GraphAPI', function() {
 				it('should return the full edge', function() {
 					expect(facebook._getEdgePath(edgeArray)).to.equal('https://graph.facebook.com/v2.2/me/picture');
 				});
+
+				it('should not modify the original array', function() {
+					facebook._getEdgePath(edgeArray);
+					expect(edgeArray.length).to.be(2);
+				});
 			});
 
 			describe('when called with string', function() {
